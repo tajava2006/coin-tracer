e00befee62c7035ad4dc12344ce0f8a0e682d423
tajava2006 <tajava2006@gmail.com>
2024-07-29 18:09:32 +0900

    fix: backward error fix

--- src/domain/trace/trace.controller.ts
+++ src/domain/trace/trace.controller.ts
@@ -6,7 +6,11 @@ export class TraceController {
   constructor(private readonly traceService: TraceService) {}
 
   @Get()
-  dfs(@Query() query: { txid: string; forward: boolean; limit: number }) {
-    return this.traceService.bfs(query.txid, query.forward, query.limit);
+  dfs(@Query() query: { txid: string; forward: string; limit: number }) {
+    return this.traceService.bfs(
+      query.txid,
+      query.forward === 'true',
+      query.limit,
+    );
   }
 }
